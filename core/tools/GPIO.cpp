36a4085485b2e2ea1543fb90830a2d38c1b2618f
ggallagh <ggallagh@sfu.ca>
2016-11-04 01:18:22 -0700

    add pwm, move to separate folder

---
+++ core/tools/GPIO.cpp
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+
+int exportGPIO(int pinNumber)
+{
+	// Use fopen() to open the file for write access.
+	FILE *pfile = fopen("/sys/class/gpio/export", "w");
+	if (pfile == NULL) {
+	printf("ERROR: Unable to open export file.\n");
+	return -1;
+	}
+	// Write to data to the file using fprintf():
+	fprintf(pfile, "%d", pinNumber);
+	// Close the file using fclose():
+	fclose(pfile);
+	return 0;
+}
+
+int setDirectionIn(int pinNumber)
+{	
+ 	char fileName[50];
+ 	
+ 	sprintf(fileName, "/sys/class/gpio/gpio%d/direction", pinNumber); 
+	int returnValue = 0;
+	
+	FILE *file = fopen(fileName, "w");
+
+	if (NULL == file) {
+		printf("ERROR OPENING %s.", fileName);
+		returnValue = -1;
+	}
+	else {
+		int charWritten = fprintf(file, "%s", "in");
+		if (charWritten <= 0) { 
+			printf("ERROR WRITING DATA");
+			returnValue = -1;
+		}
+	}
+	fclose(file);
+	return returnValue;
+}
+
+
+int setDirectionOut(int pinNumber)
+{	
+ 	char fileName[50];
+ 	
+ 	sprintf(fileName, "/sys/class/gpio/gpio%d/direction", pinNumber); 
+	int returnValue = 0;
+	
+	FILE *file = fopen(fileName, "w");
+
+	if (NULL == file) {
+		printf("ERROR OPENING %s.", fileName);
+		returnValue = -1;
+	}
+	else {
+		int charWritten = fprintf(file, "%s", "out");
+		if (charWritten <= 0) { 
+			printf("ERROR WRITING DATA");
+			returnValue = -1;
+		}
+	}
+	fclose(file);
+	return returnValue;
+}
+
+
+
+int setValue(int pinNumber, int value)
+{	
+ 	char fileName[50];
+ 	
+ 	sprintf(fileName, "/sys/class/gpio/gpio%d/value", pinNumber); 
+	int returnValue = 0;
+	
+	FILE *file = fopen(fileName, "w");
+
+	if (NULL == file) {
+		printf("ERROR OPENING %s.", fileName);
+		returnValue = -1;
+	}
+	else {
+		int charWritten = fprintf(file, "%i", value);
+		if (charWritten <= 0) { 
+			printf("ERROR WRITING DATA");
+			returnValue = -1;
+		}
+	}
+	fclose(file);
+	return returnValue;
+}
+
+
+int getValue(int pinNumber)
+{
+	char fileName[50];
+ 	
+ 	sprintf(fileName, "/sys/class/gpio/gpio%d/value", pinNumber); 
+	
+	FILE *file = fopen(fileName, "r");
+	if (NULL == file) {
+		printf("ERROR: Unable to open file (%s) for read\n", fileName);
+		exit(-1);
+	}
+
+	// Read string (line)
+	const int max_length = 8;
+	char buff[max_length];
+	fgets(buff, max_length, file);
+	
+	// Close
+	fclose(file);
+	
+	return atoi(buff);
+}
+
+
+int exportPWM(int pinNumber)
+{
+//
+}
+
+
---
+++ core/tools/GPIO.h
@@ -0,0 +1,16 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+
+
+int exportGPIO(int pinNumber);
+
+int setDirectionIn(int pinNumber);
+int setDirectionOut(int pinNumber);
+
+int setValue(int pinNumber, int value);
+int getValue(int pinNumber);
+
+
+#endif
+
