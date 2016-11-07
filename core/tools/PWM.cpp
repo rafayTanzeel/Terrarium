ae16bc7a2dec859aeda687bbc8379ede2ee11e0d
ggallagh <ggallagh@sfu.ca>
2016-11-06 22:26:34 -0800

    Detect pwmchip values at runtime

--- core/tools/PWM.cpp
+++ core/tools/PWM.cpp
@@ -1,46 +1,156 @@
 #include <stdio.h>
-//#include <time.h>
-//#include <math.h>
 #include <stdbool.h>
-#include <assert.h>
 #include <stdlib.h>
-//#include <pthread.h>
 #include <map>
 #include <cstring>
+#include <unistd.h>
+#include <dirent.h>
 #include "PWM.h"
 
-static const char* SLOTS_DIR = "/sys/devices/platform/bone_capemgr/slots";
+static const char* SLOTS_FILE = "/sys/devices/platform/bone_capemgr/slots";
 
-static const char* PINMUX_DIR = "/sys/devices/platform/ocp/ocp:";
-static const char* PWM_DIR = "/sys/class/pwm";
+static const char* OCP_DIR = "/sys/devices/platform/ocp/";
+static const char* PWM_DIR = "/sys/class/pwm/";
 
-struct PinInfo
+bool PWM::_capeLoaded = false; 
+
+static const std::map<int, const char*> pinHeaderLookup = {{50,  "P9_14"},
+			   									  	   	   {51,  "P9_16"},
+				   										   {3,   "P9_21"},
+				   										   {2,   "P9_22"},
+				   										   {123, "P9_28"},
+				   										   {121, "P9_29"},
+				   										   {120, "P9_31"},
+				   										   {23,  "P8_13"},
+				   										   {22,  "P8_19"},
+				   										   {81,  "P8_34"},
+				   										   {80,  "P8_36"},
+				   										   {70,  "P8_45"},
+				   										   {71,  "P8_46"}};
+
+
+
+PWM::PWM(int pinNumber)
 {
-	int pinNumber;
-	char headerNumber[6];
-	int pwmChipNumber;
-	int exportNumber;
-};
-
-static const std::map<int, PinInfo> pinInfoLookup = {{50,  PinInfo{50,  "P9_14", 4, 1}},
-			   									  	 {51,  PinInfo{51,  "P9_16", 6, 0}},
-			   										 {3,   PinInfo{3,   "P9_21", 0, 0}},
-			   										 {2,   PinInfo{2,   "P9_22", 2, 0}},
-			   										 {123, PinInfo{123, "P9_28", 0, 0}},
-			   										 {121, PinInfo{121, "P9_29", 0, 0}},
-			   										 {120, PinInfo{120, "P9_31", 0, 0}},
-			   										 {23,  PinInfo{23,  "P8_13", 0, 0}},
-			   										 {22,  PinInfo{22,  "P8_19", 4, 0}},
-			   										 {81,  PinInfo{81,  "P8_34", 0, 0}},
-			   										 {80,  PinInfo{80,  "P8_36", 0, 0}},
-			   										 {70,  PinInfo{70,  "P8_45", 0, 0}},
-			   										 {71,  PinInfo{71,  "P8_46", 0, 0}}};
+	_pinNumber = pinNumber;
+	initPinFS();
+	configPinMux();
+	exportPWM();
+}
 
 
-int loadCape(const char* capeName)
+
+// This method heavily based off of code provided by Pierre-Louis Constant at: https://groups.google.com/d/msg/beagleboard/gL-IobsCJ_s/p3BvBZaBBgAJ
+int PWM::initPinFS()
 {
+	loadCape("cape-universaln");
+
+
+	if (pinHeaderLookup.end() == pinHeaderLookup.find(_pinNumber)) {
+		printf("ERROR: Invalid pin number specified\n");
+		return -1;
+	}
+	const char* headerNumber = pinHeaderLookup.at(_pinNumber);
+
+    //Find chip name
+    std::string chipAddress;
+    std::string pwmNumber;
+    //EHRPWM0A
+    if (headerNumber == "P9_22" || headerNumber == "P9_31") {
+        chipAddress = "48300200.ehrpwm";
+        _exportNumber = 0;
+        pwmNumber = "pwm0";
+        }
+    //EHRPWM0B
+    if (headerNumber == "P9_21" || headerNumber == "P9_29") {
+        chipAddress = "48300200.ehrpwm";
+        _exportNumber = 1;
+        pwmNumber = "pwm1";
+        }
+    //EHRPWM1A
+    if (headerNumber == "P9_14" || headerNumber == "P8_36") {
+        chipAddress = "48302200.ehrpwm";
+        _exportNumber = 0;
+        pwmNumber = "pwm0";
+        }
+    //EHRPWM1B
+    if (headerNumber == "P9_16" || headerNumber == "P8_34") {
+        chipAddress = "48302200.ehrpwm";
+        _exportNumber = 1;
+        pwmNumber = "pwm1";
+        }
+    //EHRPWM2A
+    if (headerNumber == "P8_19" || headerNumber == "P8_45") {
+        chipAddress = "48304200.ehrpwm";
+        _exportNumber = 0;
+        pwmNumber = "pwm0";
+        }
+    //EHRPWM2B
+    if (headerNumber == "P8_13" || headerNumber == "P8_46") {
+        chipAddress = "48304200.ehrpwm";
+        _exportNumber = 1;
+        pwmNumber = "pwm1";
+                }
+    //ECAPPWM0
+    if (headerNumber == "P9_42") {
+        chipAddress = "48300100.ecap";
+        _exportNumber = 0;
+        pwmNumber = "pwm0";
+        }\
+    //EHCAPPWM2
+    if (headerNumber == "P9_28") {
+        chipAddress = "48300200.ecap";
+        _exportNumber = 0;
+        pwmNumber = "pwm0";
+        }
+               
+    std::string chipPath;
+    
+    chipPath = std::string(OCP_DIR) + "subsystem/devices/" + chipAddress +"/pwm/";
+    
+    _pwmChip = getFullNameOfFileInDirectory(chipPath, "pwmchip");
+    
+    std::string pinInterfacePath = std::string(PWM_DIR) + _pwmChip + "/";
+    
+    _dutyCyclePath = pinInterfacePath + "duty_cycle";
+    _periodPath = pinInterfacePath + "period";
+    _polarityPath = pinInterfacePath + "polarity";
+    _enablePath = pinInterfacePath + "enable";
+
+}
+
+
+
+// This method is taken from https://github.com/SaadAhmad/beaglebone-black-cpp-PWM/blob/master/library/PWM.cpp
+// A bunch of helper functions to get us locations in the file system.
+// They are used so that we can manipulate the pwm driver through the /sys interface
+std::string PWM::getFullNameOfFileInDirectory(const std::string & dirName, const std::string & fileNameToFind)
+{
+	DIR *pDir;
+
+	dirent *pFile;
+	if ((pDir = opendir(dirName.c_str())) == NULL)
+	{
+		printf("Directory doesn't exist\n");
+		throw std::bad_exception();
+	}
+	while ((pFile = readdir(pDir)) != NULL)
+	{
+		std::string currentFileName = (pFile->d_name);
+		if (currentFileName.find(fileNameToFind) != std::string::npos)
+		{
+			return currentFileName;
+		}
+	}
+	return std::string("");
+}
+
+
+int PWM::loadCape(const char* capeName)
+{
+	if (!_capeLoaded) {
 	// Use fopen() to open the file for write access.
-	FILE *pfile = fopen(SLOTS_DIR, "w");
+	FILE *pfile = fopen(SLOTS_FILE, "w");
 	if (pfile == NULL) {
 	printf("ERROR: Unable to open cape file.\n");
 	return -1;
@@ -52,20 +162,23 @@ int loadCape(const char* capeName)
 	}
 	// Close the file using fclose():
 	fclose(pfile);
+	_capeLoaded = true;
+	}
 	return 0;
 }
 
-int configPinMux(int pinNumber) {
-	if (pinInfoLookup.end() == pinInfoLookup.find(pinNumber)) {
+
+int PWM::configPinMux() {
+	if (pinHeaderLookup.end() == pinHeaderLookup.find(_pinNumber)) {
 		printf("ERROR: Invalid pin number specified\n");
 		return -1;
 	}
-	const char* headerNumber = pinInfoLookup.at(pinNumber).headerNumber;
+	const char* headerNumber = pinHeaderLookup.at(_pinNumber);
 	
 	// Use fopen() to open the file for write access.
 	char fileName[50];
  	
- 	sprintf(fileName, "%s%s_pinmux/state", PINMUX_DIR, headerNumber); 
+ 	sprintf(fileName, "%socp:%s_pinmux/state", OCP_DIR, headerNumber); 
 	
 	FILE *pfile = fopen(fileName, "w");
 	if (pfile == NULL) {
@@ -83,19 +196,17 @@ int configPinMux(int pinNumber) {
 }
 
 
-int exportPWM(int pinNumber)
+int PWM::exportPWM()
 {
-	if (pinInfoLookup.end() == pinInfoLookup.find(pinNumber)) {
+	if (pinHeaderLookup.end() == pinHeaderLookup.find(_pinNumber)) {
 		printf("ERROR: Invalid pin number specified\n");
 		return -1;
 	}
-	
-	int pwmChipNumber = pinInfoLookup.at(pinNumber).pwmChipNumber;
-	int exportNumber = pinInfoLookup.at(pinNumber).exportNumber;
+	const char* headerNumber = pinHeaderLookup.at(_pinNumber);
 	
 	char fileName[50];
  	
- 	sprintf(fileName, "%s/pwmchip%i/export", PWM_DIR, pwmChipNumber); 
+ 	sprintf(fileName, "%s%s/export", PWM_DIR, _pwmChip.c_str()); 
 	
 	
 	// Use fopen() to open the file for write access.
@@ -105,9 +216,7 @@ int exportPWM(int pinNumber)
 		return -1;
 	}
 	// Write to data to the file using fprintf():	
-	char exportNumString[4];
-	sprintf(exportNumString, "%d", exportNumber);
-	if (strlen(exportNumString) != fprintf(pfile, "%s", exportNumString)) {
+	if (1 != fprintf(pfile, "%i", _exportNumber)) {
 		printf("ERROR: Unable to export pin.\n");
 		return -1;
 	}
@@ -117,21 +226,18 @@ int exportPWM(int pinNumber)
 }
 
 
-int setDutyCycle(int pinNumber, int value)
+int PWM::setDutyCycle(int nanoseconds)
 {
- 	char fileName[50];
- 	
- 	sprintf(fileName, "/sys/class/pwm/pwm%d/value", pinNumber); 
 	int returnValue = 0;
 	
-	FILE *file = fopen(fileName, "w");
+	FILE *file = fopen(_dutyCyclePath.c_str(), "w");
 
 	if (NULL == file) {
-		printf("ERROR OPENING %s.", fileName);
+		printf("ERROR OPENING %s.", _dutyCyclePath.c_str());
 		returnValue = -1;
 	}
 	else {
-		int charWritten = fprintf(file, "%i", value);
+		int charWritten = fprintf(file, "%i", nanoseconds);
 		if (charWritten <= 0) { 
 			printf("ERROR WRITING DATA");
 			returnValue = -1;
@@ -142,21 +248,18 @@ int setDutyCycle(int pinNumber, int value)
 
 }
 
-int setPeriod(int pinNumber, int value)
+int PWM::setPeriod(int nanoseconds)
 {
- 	char fileName[50];
- 	
- 	sprintf(fileName, "/sys/class/gpio/gpio%d/value", pinNumber); 
 	int returnValue = 0;
 	
-	FILE *file = fopen(fileName, "w");
+	FILE *file = fopen(_periodPath.c_str(), "w");
 
 	if (NULL == file) {
-		printf("ERROR OPENING %s.", fileName);
+		printf("ERROR OPENING %s.", _periodPath.c_str());
 		returnValue = -1;
 	}
 	else {
-		int charWritten = fprintf(file, "%i", value);
+		int charWritten = fprintf(file, "%i", nanoseconds);
 		if (charWritten <= 0) { 
 			printf("ERROR WRITING DATA");
 			returnValue = -1;
@@ -167,14 +270,14 @@ int setPeriod(int pinNumber, int value)
 
 }
 
-int getDutyCycle(int pinNumber)
+int PWM::getDutyCycle()
 {
 
 
 }
 
 
-int getPeriod(int pinNumber)
+int PWM::getPeriod()
 {
 
 
@@ -184,9 +287,7 @@ int getPeriod(int pinNumber)
 
 int main()
 {
-	loadCape("cape-universaln");
-	configPinMux(50);
-	exportPWM(50);
+	PWM pwm50(50);
 
 }
 
--- core/tools/PWM.h
+++ core/tools/PWM.h
@@ -10,6 +10,47 @@ int setPeriod(int pinNumber, int value);
 int getDutyCycle(int pinNumber);
 int getPeriod(int pinNumber);
 
+class PWM
+
+{
+
+public:
+	
+	PWM(int pinNumber);
+	
+	int setDutyCycle(int value);
+	int setPeriod(int value);
+
+	int getDutyCycle();
+	int getPeriod();
+		
+private:
+
+	int initPinFS();
+	
+	static int loadCape(const char* capeName);
+	
+	std::string getFullNameOfFileInDirectory(const std::string & dirName, const std::string & fileNameToFind);
+	
+	int configPinMux();
+	
+	int exportPWM();
+
+	static bool _capeLoaded;
+
+	int _pinNumber;
+	int _exportNumber;
+	
+	//char headerNumber[6];
+	
+	std::string _pwmChip;
+	
+	std::string _dutyCyclePath;
+	std::string _periodPath;   
+	std::string _polarityPath;
+	std::string _enablePath;
+};
+
 
 /*50 = 9_14 = 3
 
